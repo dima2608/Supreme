201e027de15567ef1f50f1678ca0775760ca7929
Dima Danilov <ddvdani14@gmail.com>
2022-04-22 20:24:42 +0300

    initial commit

---
+++ app/src/main/java/com/triare/supreme/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.triare.supreme.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.triare.supreme.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
