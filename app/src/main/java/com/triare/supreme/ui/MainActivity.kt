da5794253b0e62799d51ecd3cc0e3dde4957db38
Dima Danilov <ddvdani14@gmail.com>
2022-04-23 19:34:28 +0300

    add: bottomNav

--- app/src/main/java/com/triare/supreme/ui/MainActivity.kt
+++ app/src/main/java/com/triare/supreme/ui/MainActivity.kt
@@ -3,10 +3,30 @@ package com.triare.supreme.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.triare.supreme.R
+import com.triare.supreme.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initUi()
+    }
+
+    private fun initUi() {
+        initBottomNav()
+    }
+
+    private fun initBottomNav() {
+        binding.bottomNav.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.news -> true
+                R.id.racing -> true
+                R.id.standings -> true
+            }
+            true
+        }
     }
 }
